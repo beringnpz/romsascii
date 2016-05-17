ff41f94b79435f40ad5d2a47d0c150b70b90dad3
Kelly Kearney <kakearney@gmail.com>
2016-05-17 12:25:12 -0700

    Removing install_required for some testing

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version='0.1dev',
     packages=['romsascii'],
     author='Kelly Kearney',
-    author_email='kakearney@gmail.com',
-    install_requires=['collections', 'datetime', 'os', 'subprocess', 'copy', 'numpy', 're']
+    author_email='kakearney@gmail.com'
+    # install_requires=['collections', 'datetime', 'os', 'subprocess', 'copy', 'numpy', 're']
 )
