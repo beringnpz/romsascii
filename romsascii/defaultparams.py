9df40995ea321464dda35eb90c4a5aec2873a3e6
Kelly Kearney <kakearney@gmail.com>
2016-07-25 11:21:21 -0700

    Dictionary merge function now returns OrderedDict

--- romsascii/defaultparams.py
+++ romsascii/defaultparams.py
@@ -788,7 +788,7 @@ def merge_dicts(*dict_args):
     Given any number of dicts, shallow copy and merge into a new dict,
     precedence goes to key value pairs in latter dicts.
     '''
-    result = {}
+    result = OrderedDict()
     for dictionary in dict_args:
         result.update(dictionary)
     return result
